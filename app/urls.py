e1ae2e882cae2ded3ad50d0d0424df1e00c9022d
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-07 22:41:51 -0300

    feat : "templates setup"

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import IndexView
+from .views import (
+    IndexView, SobreDetailView, ServicoDetailView,
+    PrecoDetailView, ContatoDetailView
+)
 
 urlpatterns = [
-    path('home/', IndexView.as_view())
+    path('home/', IndexView.as_view(), name='home'),
+    path('sobre/', SobreDetailView.as_view(), name='sobre-datail'),
+    path('servico/', ServicoDetailView.as_view(), name='servico-detail'),
+    path('precos/', PrecoDetailView.as_view(), name='preco-detail'),
+    path('contato/', ContatoDetailView.as_view(), name='contato-detail')
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,19 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'app/pages/index.html'
+
+
+class SobreDetailView(TemplateView):
+    template_name = 'app/sobre-detail.html'
+
+
+class ServicoDetailView(TemplateView):
+    template_name = 'app/servico-detail.html'
+
+
+class PrecoDetailView(TemplateView):
+    template_name = 'app/preco-detail.html'
+
+
+class ContatoDetailView(TemplateView):
+    template_name = 'app/contato-detail.html'
